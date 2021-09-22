4f79593fb91b0aadb8bac14e830dc1d3a8e4f040
Alex Binnie <alexjbinnie@gmail.com>
2021-09-22 15:35:46 +0100

    Fixed import order, force OpenMM < 7.6.0

--- narupatools/src/narupatools/mdtraj/__init__.py
+++ narupatools/src/narupatools/mdtraj/__init__.py
@@ -17,7 +17,7 @@
 """Utilities for interfacing with the MDTraj package."""
 
 from .converter import mdtraj_topology_to_frame, mdtraj_trajectory_to_frame
-from .units import UnitsMDTraj
 from .trajectory import MDTrajTrajectory
+from .units import UnitsMDTraj
 
 __all__ = ["UnitsMDTraj", "mdtraj_trajectory_to_frame", "mdtraj_topology_to_frame", "MDTrajTrajectory"]
