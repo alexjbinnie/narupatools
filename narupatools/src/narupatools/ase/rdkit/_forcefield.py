Fix converter, remove arbitrary scaling of RDKit force

narupatools/src/narupatools/ase/rdkit/_forcefield.py
@@ -54,7 +54,7 @@ class _RDKitForcefield:
             )
         else:
             forces = self._forcefield.CalcGrad()
-        return -np.array(forces).reshape((-1, 3)) * _RDKitToNarupa.force * 0.00001  # type: ignore[no-any-return]
+        return -np.array(forces).reshape((-1, 3)) * _RDKitToNarupa.force  # type: ignore[no-any-return]
 
 
 class _MMFFForceField(_RDKitForcefield):