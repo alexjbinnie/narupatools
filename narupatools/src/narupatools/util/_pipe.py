73f5f070b2a02fd002cc5b3846dc200538afd0b9
Alex Binnie <alexjbinnie@gmail.com>
2022-04-19 10:03:04 +0100

    Remove unused type ignore

--- narupatools/src/narupatools/util/_pipe.py
+++ narupatools/src/narupatools/util/_pipe.py
@@ -27,7 +27,7 @@ class PipedOutput:
 
     def __init__(self, handle_line: Any):
         self._filename = os.path.join(tempfile.mkdtemp(), "fifo")
-        os.mkfifo(self.filename)  # type: ignore
+        os.mkfifo(self.filename)
         self._thread = Thread(target=self._thread_run, daemon=True)
         self._thread.start()
         self._handle_line = handle_line
